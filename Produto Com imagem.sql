2be56bfbfd32451d013a5405d1188945c44ad61d
Kelven Cardoso <84426581+kelven-cardoso@users.noreply.github.com>
2022-05-02 10:43:24 -0300

    aTUALIZAÇÃO
    aDICIONADO FORNECEDOR

--- Produto Com imagem.sql
+++ Produto Com imagem.sql
@@ -1,17 +1,21 @@
 SELECT
   pcprodut.CODPROD,
   Upper(pcprodut.DESCRICAO),
+  PCFORNEC.FORNECEDOR,
   PCTABPR.PTABELA,
   pcprodut.DIRFOTOPROD,
   :DIGITE_SEU_NOME,
   replace(:DESCRICAO,'%','')
 FROM
   pcprodut,
-  PCTABPR
+  PCTABPR,
+  PCFORNEC
 WHERE
   PCTABPR.CODPROD = pcprodut.CODPROD
+  and pcprodut.codfornec = PCFORNEC.codFORNEC
   AND pcprodut.DESCRICAO LIKE :DESCRICAO
   AND pcprodut.CODPROD LIKE :CODIGO_PRODUTO
+  and pcfornec.CODFORNEC in (:fornecedor)
   AND PCTABPR.NUMREGIAO = 1
   AND PCTABPR.PTABELA IS NOT NULL
 
